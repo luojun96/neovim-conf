128cf5b98b6518f17719db201bdf5039a91eee9c
luo jun <luojun96@live.cn>
2023-03-14 21:20:09 +0800

    add configurations

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nervim-conf
 Customized Configuration for Neovim
+
+This configuration for Neovim references [Neovim](https://github.com/MarioCarrion/videos/tree/main/2023/01/nvim)
---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,40 @@
+local setup, nvimtree = pcall(require, "nvim-tree")
+if not setup then return end
+
+vim.cmd([[
+  nnoremap - :NvimTreeToggle<CR>
+]])
+
+-- local keymap = vim.keymap -- for conciseness
+-- keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.opt.foldenable = false --                  " Disable folding at startup.
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
+local HEIGHT_RATIO = 0.8 -- You can change this
+local WIDTH_RATIO = 0.5  -- You can change this too
+
+nvimtree.setup({
+  disable_netrw = true,
+  hijack_netrw = true,
+  respect_buf_cwd = true,
+  sync_root_with_cwd = true,
+  view = {
+    relativenumber = false,
+    side = "left",
+    width = 30,
+  },
+  filters = {
+    custom = { "^.git$" },
+  },
+  renderer = {
+    indent_width = 1,
+  },
+})
+
