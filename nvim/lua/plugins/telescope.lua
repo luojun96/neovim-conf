b4eb00c4c7d1c111019d133d1db00b7800353bac
Jun Luo <luojun96@live.cn>
2023-12-14 08:51:39 +0800

    add some shutcuts for Go.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -24,3 +24,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fx', builtin.treesitter, {})
+-- go
+vim.keymap.set('n', '<leader>fl', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>fld', builtin.lsp_workspace_symbols , {})
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references , {})
